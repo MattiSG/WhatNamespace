a38367b9d4288295a439b038aea83d21210441ce
Matti Schneider-Ghibaudo <matti.schneiderghibaudo@gmail.com>
2012-01-04 10:36:25 +0100

    Added tracker.

--- index.php
+++ index.php
@@ -43,5 +43,17 @@
 		
 		<script src="https://ajax.googleapis.com/ajax/libs/mootools/1.4.1/mootools-yui-compressed.js" language="javascript" type="text/javascript"></script>
 		<script src="js/WhatNamespace.js" language="javascript" type="text/javascript"></script>
+		
+		<script type="text/javascript">
+			var _gaq = _gaq || [];
+			_gaq.push(['_setAccount', 'UA-28093627-1']);
+			_gaq.push(['_trackPageview']);
+			
+			(function() {
+			var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+			ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+			var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+			})();
+		</script>
 	</body>
 </html>
