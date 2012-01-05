4e48ee9c343d6e3c35cb601a3865020aea62585c
Matti Schneider-Ghibaudo <matti.schneiderghibaudo@gmail.com>
2012-01-05 19:17:09 +0100

    “tiny, one letter-edit”

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <!doctype html>  
 <html lang="en-EN">
 	<head>
-		<meta charset="utf-8">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
+		<meta charset="UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 		<meta name="viewport" content="width=device-width, user-scalable=no">
 	
 		<title>XML dialects and their namespaces &#8212; WhatNamespace</title>
