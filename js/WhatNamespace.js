2f1ddf2845f6273097cbda0a0a8df1e564840352
Matti Schneider-Ghibaudo <matti.schneiderghibaudo@gmail.com>
2012-01-05 22:44:32 +0100

    Added history support.

--- js/WhatNamespace.js
+++ js/WhatNamespace.js
@@ -3,14 +3,24 @@ window.addEvent('domready', function() {
 	
 	$$('.entry').each(function(tr) {
 		var nsInput = tr.getElement('input');
+		
 		var selecter = function() {
 			nsInput.focus();	// two stages are necessary for mobile Safari
 			nsInput.select();
 		};
-		tr.addEvent(
-					('ontouchstart' in window ? 'click' : 'mouseenter'), // for mobile, mouseenter fires and exits immediately due to keyboard. Can't use touchstart though (overrides scrolling).
-					selecter
-		).addClass('active');	// for CSS styling
+		
+		tr.addEvent('click', selecter)
+		  .addClass('active');	// for CSS styling
+		
+		if (! ('ontouchstart' in window))
+			tr.addEvent('mouseenter', selecter);  // for mobile, mouseenter fires and exits immediately due to keyboard. Can't use touchstart though (overrides scrolling).
+		
+		if (history) {
+			tr.addEvent('click', function() {
+				var id = tr.getElement('th').get('id');
+				history.replaceState(null, id, '#' + id);
+			});
+		}
 		
 		nsInput.addEvent('focus', tr.addClass.pass(HOVER_CLASS, tr))
 			   .addEvent('blur', tr.removeClass.pass(HOVER_CLASS, tr));
