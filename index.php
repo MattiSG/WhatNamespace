e38d64a424103d8f5f45ad8d5e41a14a56d8baf9
Matti Schneider <hi@mattischneider.fr>
2014-05-26 21:36:41 +0100

    Fix whitespace

--- index.php
+++ index.php
@@ -1,60 +1,60 @@
-<!doctype html>  
+<!doctype html>
 <html lang="en-EN" manifest="cache.manifest">
 	<head>
 		<meta charset="UTF-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 		<meta name="viewport" content="width=device-width, user-scalable=no">
-	
+
 		<title>XML dialects and their namespaces &#8212; WhatNamespace</title>
 		<meta name="description" content="XML dialects and their namespaces.">
 		<meta name="author" content="Matti Schneider-Ghibaudo">
-		
+
 		<link rel="stylesheet" type="text/css" href="css/main.css"/>
 	</head>
-	
+
 	<body>
-	    <header>
-	    	<!--googleoff: snippet-->
+		<header>
+			<!--googleoff: snippet-->
 			<h1>WhatNamespace</h1>
 			<!--googleon: snippet-->
 			<p>A directory of XML dialects and their namespaces.</p>
 			<!--googleoff: snippet-->
 			<p><a href="http://www.rpbourret.com/xml/NamespacesFAQ.htm#exec_1" target="_blank">What is a namespace?</a></p>
 			<!--googleon: snippet-->
-	    </header>
-	    
-	    <div id="main" role="main">
-	    	<table>
-	    		<thead>
-	    			<tr>
-	    				<th>Dialect</th>
-	    				<th>Namespace</th>
-	    				<th>Spec</th>
-	    			</tr>
-	    		</thead>
-	    		
-	    		<tbody>
-	    			<?php
-	    				include(dirname(__FILE__) . '/namespacesTable.php');
-	    			?>
-	    		</tbody>
-	    	</table>
+		</header>
+
+		<div id="main" role="main">
+			<table>
+				<thead>
+					<tr>
+						<th>Dialect</th>
+						<th>Namespace</th>
+						<th>Spec</th>
+					</tr>
+				</thead>
+
+				<tbody>
+					<?php
+						include(dirname(__FILE__) . '/namespacesTable.php');
+					?>
+				</tbody>
+			</table>
 		</div>
-		  
+
 		<footer>
 			<a href="http://www.w3.org/TR/REC-xml-names/">XML Namespace Spec</a>
 			—
 			Made by <a href="http://mattischneider.fr">Matti Schneider-Ghibaudo</a>
 		</footer>
-		
+
 		<script src="js/mootools-core-1.4.2.js" language="javascript" type="text/javascript"></script> <!-- no CDN for proper app cache (FALLBACK has same origin policy) -->
 		<script src="js/WhatNamespace.js" language="javascript" type="text/javascript"></script>
-		
+
 		<script type="text/javascript">
 			var _gaq = _gaq || [];
 			_gaq.push(['_setAccount', 'UA-28093627-1']);
 			_gaq.push(['_trackPageview']);
-			
+
 			(function() {
 			var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
 			ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
