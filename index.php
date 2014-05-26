9e59d7314314dd73896a5346c4352ab5409bb100
Matti Schneider <hi@mattischneider.fr>
2014-05-26 21:37:28 +0100

    Remove Google Analytics
    Stop feeding a private silo with user-tracking data

--- index.php
+++ index.php
@@ -49,17 +49,5 @@
 
 		<script src="js/mootools-core-1.4.2.js" language="javascript" type="text/javascript"></script> <!-- no CDN for proper app cache (FALLBACK has same origin policy) -->
 		<script src="js/WhatNamespace.js" language="javascript" type="text/javascript"></script>
-
-		<script type="text/javascript">
-			var _gaq = _gaq || [];
-			_gaq.push(['_setAccount', 'UA-28093627-1']);
-			_gaq.push(['_trackPageview']);
-
-			(function() {
-			var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-			ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-			var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-			})();
-		</script>
 	</body>
 </html>
